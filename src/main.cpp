2153fbe9e1de3eabbad38d8fee88334d673a00c9
PatelTirth25 <tirthpatel204@gmail.com>
2026-02-22 15:32:36 +0530

    feat: file input taken from cmd

--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,33 @@
 #include "http_client.h"
 #include <iostream>
 
-int main() {
+int main(int argc, char *argv[]) {
+  // Check if the user provided exactly one argument
+  if (argc != 2) {
+    std::cerr << "Usage Error: Missing configuration file.\n";
+    std::cout << "Usage: ./bot <config_file.json>\n";
+    return 1;
+  }
+
+  // Use the argument from the command line instead of a hardcoded string
+  std::string configFilePath = argv[1];
+
   try {
-    // Parse the JSON config file
-    ConfigParser parser("../example.json");
+    ConfigParser parser(configFilePath);
     BotConfig config = parser.parse();
 
     std::cout << "Bot name: " << config.bot_name << "\n";
 
-    // Perform HTTP requests via below struct
     HttpClient client;
 
     for (const auto &req : config.requests) {
       std::cout << "\nFetching [" << req.name << "] " << req.url << "...\n";
 
+      // The GET request happens here
       std::string html = client.get(req.url);
 
-      std::cout << "Fetched: " << html.size() << " bytes from " << req.url
-                << " Content: \n"
-                << html << "\n";
+      std::cout << "Fetched " << html.size() << " bytes from " << req.url
+                << "\n";
     }
   } catch (const std::exception &ex) {
     std::cerr << "Error: " << ex.what() << "\n";
